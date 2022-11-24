acf1c3ddd6fbb15c8729654e3fced93993b0b494
DK <dongwook5478@gmail.com>
2022-11-24 18:37:09 +0900

    edit menu

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 
-user_Checker=True
-while(user_Checker):
-    print("ID :")
-    user_ID = input()
-    print("PW :")
-    user_PW = input()
+# user_Checker=True
+# while(user_Checker):
+#     print("ID :")
+#     user_ID = input()
+#     print("PW :")
+#     user_PW = input()
+#
+#     if (user_ID=="dong5478") & (user_PW=="ehddnr0428"):
+#         print(user_ID, user_PW)
+#         user_Checker=False
+#     else:
+#         print("please insert right number")
 
-    if (user_ID=="dong5478") & (user_PW=="ehddnr0428"):
-        print(user_ID, user_PW)
-        user_Checker=False
-    else:
-        print("please insert right number")
+print("what you wanna do")
+menu = input("1. nu customer  2. items")
+if (menu=="1") | ("nu" in menu):
+    print("yessss!")
