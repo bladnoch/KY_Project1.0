933f02441ff5fccfcd6a56191b22f74becb16f86
DK <dongwook5478@gmail.com>
2022-11-24 18:04:58 +0900

    creat id,pw checker

---
+++ main.py
@@ -0,0 +1,13 @@
+
+user_Checker=True
+while(user_Checker):
+    print("ID :")
+    user_ID = input()
+    print("PW :")
+    user_PW = input()
+
+    if (user_ID=="dong5478") & (user_PW=="ehddnr0428"):
+        print(user_ID, user_PW)
+        user_Checker=False
+    else:
+        print("please insert right number")
