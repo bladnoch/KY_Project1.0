de3c2b2e52ec9cf0f299ef252d76774966975599
DK <dongwook5478@gmail.com>
2022-11-24 20:03:20 +0900

    edit idpw on class

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+class Userchecker:
+    def __init__(self):
+        user_Checker = True
+        while (user_Checker):
+            print("ID :")
+            user_ID = input()
+            print("PW :")
+            user_PW = input()
 
-user_Checker=True
-while(user_Checker):
-    print("ID :")
-    user_ID = input()
-    print("PW :")
-    user_PW = input()
+            if (user_ID == "dong5478") & (user_PW == "ehddnr0428"):
+                print(user_ID, user_PW)
+                user_Checker = False
+            else:
+                print("please insert right number")
 
-    if (user_ID=="dong5478") & (user_PW=="ehddnr0428"):
-        print(user_ID, user_PW)
-        user_Checker=False
-    else:
-        print("please insert right number")
+
+admin=Userchecker()
+
+# user_Checker=True
+# while(user_Checker):
+#     print("ID :")
+#     user_ID = input()
+#     print("PW :")
+#     user_PW = input()
+#
+#     if (user_ID=="dong5478") & (user_PW=="ehddnr0428"):
+#         print(user_ID, user_PW)
+#         user_Checker=False
+#     else:
+#         print("please insert right number")
