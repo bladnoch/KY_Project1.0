b02c1352c674e1f339959e49a7ef67fc4207d5f9
DK <dongwook5478@gmail.com>
2022-11-25 17:49:53 +0900

    add menu and edit menu, money

--- main.py
+++ main.py
@@ -30,14 +30,24 @@ class Money:
         Money.payed=ppayed
     def setChange(self,cchange):
         Money.change=cchange
+
+
+
 #메뉴
 class Menu:
     tf=True
     def __init__(self): #menu 생성자(Constructor)
 
         while(Menu.tf):
-            print("what you wanna do")
-            menu = input("1. nu customer  2. items 3. how much 4. over")
+            print(">>----------------------------")
+            print("id","고인명","상주명","빈소")
+            print("빈소기간","안치기간")
+            print("수닙금액 :",Money.pay,"   받은 :",Money.payed,"   거스름돈 :",Money.change)
+            print("items")
+            print("------------------------------")
+
+            menu = input("1. nu customer  2. items 3. how much 4. over \n")
+            print("------------------------------")
             if (menu == "1") | ("nu" in menu) | ("customer" in menu):
                 print("1 nu customer")
             elif (menu == "2") | ("items" in menu):
@@ -48,6 +58,7 @@ class Menu:
                 self.Cm=Money()
                 self.Cm.setPayed(self.payed)
                 print("거스름돈 : ", Money.pay-Money.payed)
+                self.Cm.setChange(Money.pay-Money.payed)
             else:
                 print("log off...")
                 Menu.tf=False
@@ -55,7 +66,7 @@ class Menu:
 
 
 if __name__ == '__main__':
-    admin=Admin()
+    # admin=Admin()
     menu=Menu()
     money=Money()
 
