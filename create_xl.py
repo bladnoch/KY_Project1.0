fa5b53f5358afc0c95b1e4041c87e2e0a09da191
DK <dongwook5478@gmail.com>
2022-11-28 00:33:45 +0900

    add etc

--- create_xl.py
+++ create_xl.py
@@ -9,6 +9,13 @@ ws=wb.create_sheet("워크시트 만들기")
 
 #데이터 추가하기
 #test.xlsx를 불러 올수 있으면 불러와서 이곳에 추가
+#자동으로 위치를 지정하는 방법이 있어야함
+test="A"
+test2=1
+for i in range:
+    test2=i
+    test3=test+test2
+    print(test3)
 ws['A1']='1'
 ws['B1']='물품코드'
 ws['C1']='물품명'
@@ -27,4 +34,5 @@ ws['G1']=0
 
 
 #엑셀 저장
+# !엑셀 이름을 어떻게 줄것인지 생각해야함 ->int변수나 id를 이름으로 주는 방법
 wb.save('/Users/doungukkim/Desktop/workspace/python/restinpeace/excelhere/createXl.xlsx')
---
+++ etc.py
@@ -0,0 +1,23 @@
+#숫자 변환
+test="A"
+test2=1
+for i in range (10):
+    test2=i
+    test3=test+str(test2)
+    print(test3)
+print("")
+#영어 변환
+tt=['A','B','C','D','E','F','G','H']
+tt2=1
+for i in range(8):
+    tt3=tt[i]+str(tt2)
+    print(tt3)
+
+# at=[]
+# at2=[]
+# for i in range(10): #숫자파트
+#     at.append()
+#     for j in range(8):
+
+
+
