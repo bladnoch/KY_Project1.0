da637afa975829049808d22fb92c7953314675f3
DK <dongwook5478@gmail.com>
2022-11-28 01:50:45 +0900

    no clue...

--- etc.py
+++ etc.py
@@ -1,17 +1,17 @@
-#숫자 변환
-test="A"
-test2=1
-for i in range (10):
-    test2=i
-    test3=test+str(test2)
-    print(test3)
-print("")
-#영어 변환
-tt=['A','B','C','D','E','F','G','H']
-tt2=1
-for i in range(8):
-    tt3=tt[i]+str(tt2)
-    print(tt3)
+# #숫자 변환
+# test="A"
+# test2=1
+# for i in range (10):
+#     test2=i
+#     test3=test+str(test2)
+#     print(test3)
+# print("")
+# #영어 변환
+# tt=['A','B','C','D','E','F','G','H']
+# tt2=1
+# for i in range(8):
+#     tt3=tt[i]+str(tt2)
+#     print(tt3)
 
 # at=[]
 # at2=[]
@@ -19,5 +19,54 @@ for i in range(8):
 #     at.append()
 #     for j in range(8):
 
+from .models import Article
 
+
+class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+
+        max_length=100,
+
+        label='제목',
+
+        help_text='제목은 100자이내로 작성하세요.',
+
+        widget=forms.TextInput(
+
+            attrs={
+
+                'class': 'my-input',
+
+                'placeholder': '제목 입력'
+
+            }
+
+        )
+
+    )
+
+    content = forms.CharField(
+
+        label='내용',
+
+        help_text='자유롭게 작성해주세요.',
+
+        widget=forms.Textarea(
+
+            attrs={
+
+                'row': 5,
+
+                'col': 50,
+
+            }
+
+        )
+
+    )
+
+    class Meta:
+        model = Article
+
+        fields = ['title', 'content']
 
--- using_xl.py
+++ using_xl.py
@@ -1,21 +1,22 @@
 import openpyxl
 
 #주소 변수에 저장
-home="/Users/doungukkim/Desktop/workspace/python/restinpeace/excelhere/test.xlsx/test.xlsx"
+home="/Users/doungukkim/Desktop/workspace/python/restinpeace/excelhere/test.xlsx"
 # 엑셀 불러오기
 wb= openpyxl.load_workbook(home)
 
 # 엑셀 시트 선택
-ws=wb['sheet1']
+ws=wb['Sheet1']
 
+print(ws['A1'].value)
 #데이터 수정하기
-ws['A3']= "X"
-ws['B3']= "XXXXXXX"
-ws['C3']="테스트 숴정"
-ws['D3']= "X"
-ws['E3']= "XXX"
-ws['F3']= ""
-ws['G3']= "X"
+# ws['A3']= "X"
+# ws['B3']= "XXXXXXX"
+# ws['C3']="테스트 숴정"
+# ws['D3']= "X"
+# ws['E3']= "XXX"
+# ws['F3']= ""
+# ws['G3']= "X"
 
-#엑셀 저장
-wb.save(home)
+# #엑셀 저장
+# wb.save(home)
