b64494a92dbe829e8e647cdd929207e7fa0b8fad
DK <dongwook5478@gmail.com>
2022-12-05 03:03:17 +0900

    add add button

--- v3.py
+++ v3.py
@@ -1,8 +1,58 @@
 from tkinter import * # tkinter의 모든 함수 가져오기
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
+import subprocess
 import openpyxl
+import os
 from openpyxl.worksheet.table import Table, TableStyleInfo
 import tkinter.ttk
+
+
+# def openfile():
+def openxl():
+    def close():
+        openxl.quit()
+        openxl.destroy()
+
+    openxl=Tk()
+
+    openxl.geometry("300x170")  # 창의 크기
+    openxl.title("장례식장 재고관리 프로그램 Ver1.221123")  # 창의 제목
+    openxl.option_add("*Font", "맑은고딕 11")  # 전체 폰트
+
+    l_item = Label(openxl)
+    l_item.config(text="물품명", width=10, relief="solid")
+    l_item.place(x=20,y=20)
+
+    l_price = Label(openxl)
+    l_price.config(text="단가", width=10, relief="solid")
+    l_price.place(x=20, y=50)
+
+    l_count = Label(openxl)
+    l_count.config(text="단위", width=10, relief="solid")
+    l_count.place(x=20, y=80)
+
+    e_item = Entry(openxl)
+    e_item.config(width=20, relief="solid", borderwidth=2)
+
+    e_price = Entry(openxl)
+    e_price.config(width=20, relief="solid", borderwidth=2)
+
+    e_count = Entry(openxl)
+    e_count.config(width=20, relief="solid", borderwidth=2)
+
+    e_item.place(x=110,y=20)
+    e_price.place(x=110,y=50)
+    e_count.place(x=110,y=80)
+
+    save = Button(openxl, text="저장")
+    save.config(width=6, height=2)
+    save.place(x=60,y=115)
+
+    cancel = Button(openxl, text="취소")
+    cancel.config(width=6, height=2)
+    cancel.place(x=150,y=115)
+
+    openxl.mainloop()
 
 def close():
     win.quit()
@@ -22,14 +72,17 @@ def first():
         row = []
 
     for i in range(1,count):
-        treelist.append(og_l[i][3])
+        리스트.insert(i-1,og_l[i][2])
 
+def listitem():
+
+    messagebox(리스트.curselection())
 
 
 
 ##################################################   global variable   ##########################
 
-home = "/Users/doungukkim/Desktop/workspace/python/restinpeace/excelhere/test2.xlsx" #기본 물품 엑셀 위치 저장
+home = '/Users/doungukkim/Desktop/workspace/python/restinpeace/excelhere/test.xlsx' #기본 물품 엑셀 위치 저장
 
 room1='/Users/doungukkim/Desktop/workspace/python/restinpeace/excelhere/room_one.xlsx'
 room2='/Users/doungukkim/Desktop/workspace/python/restinpeace/excelhere/room_two.xlsx'
@@ -51,6 +104,7 @@ treelist=[]
 
 
 
+
 ##################################################   tkinter   ##########################
 win = Tk() # 창 생성
 win.geometry("1000x720") # 창의 크기
@@ -60,6 +114,8 @@ win.option_add("*Font", "맑은고딕 11") # 전체 폰트
 
 ID_lab = Label(win)
 ID_lab.config(text = "ID", width=10, relief="solid")
+물품명 = Label(win)
+물품명.config(text = "물품명", width=15, relief="solid",borderwidth=0)
 고인명_lab = Label(win)
 고인명_lab.config(text = "고인명",width=10, relief="solid")
 상주명_lab = Label(win)
@@ -92,16 +148,20 @@ ID.config(width=10,relief="solid",borderwidth=2)
 거스름돈 = Entry(win)
 거스름돈.config(width=20,relief="solid",borderwidth=2)
 
+리스트 = Listbox(win, selectmode = 'extended',width = 15, height = 30)
+리스트.yview()
 
 ##################################################   buttons   ##########################
 저장 = Button(win, text = "저장")
 저장.config(width=10,height=2)
 불러오기 = Button(win, text = "불러오기")
 불러오기.config(width=10,height=2)
-닫기 = Button(win, text = "닫기")
+닫기 = Button(win, text = "닫기",command=close)
 닫기.config(width=10,height=3)
-수정 = Button(win, text = "수정")
-수정.config(width=10,height=3)
+물품수정 = Button(win, text = "풀품수정",command=openxl)
+물품수정.config(width=7,height=2)
+
+
 삭제 = Button(win, text = "삭제")
 삭제.config(width=10,height=3)
 Set = Button(win, text = "checker")
@@ -131,12 +191,14 @@ ID.place(x=110,y=10)
 거스름돈.place(x=720,y=110)
 
 #버튼 위치
-tree.place(x=10,y=210)
+물품명.place(x=-15,y=220)
 저장.place(x= 350, y=50)
 불러오기.place(x=470,y=50)
 닫기.place(x=370, y=150)
-수정.place(x=10, y=150)
+물품수정.place(x=10, y=150)
 삭제.place(x=130, y=150)
 Set.place(x=250, y=150)
+
+리스트.place(x=20, y=240)
 
 win.mainloop() # 창 실행
