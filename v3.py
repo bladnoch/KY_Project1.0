9fc5dab4deb53f02842be27bb1f9170508990f8a
DK <dongwook5478@gmail.com>
2022-12-06 11:01:57 +0900

    add clickEvent_delete

--- v3.py
+++ v3.py
@@ -380,8 +380,18 @@ def clickEvent(event): #리스트박스 더블 클릭하면 인덱스 받아서 
     # messagebox.showinfo("treelist[i]",new_l[i])
 
     tree.place(x=170,y=210)
+    tree.bind("<Double-Button-1>", clickEvent2_delete)
     # tree.delete(*tree.get_children())
     win.update()
+def clickEvent2_delete(event):
+    messagebox.showinfo("",tree.selection_get())
+
+    for i in range(len(new_l)): #삭제될 tree 요소를 list에서도 삭제
+        if(tree.selection_get()==new_l[i][0]):
+            new_l.remove(new_l[i])
+
+    selected_item = tree.selection()[0]  ## get selected item
+    tree.delete(selected_item)
 def clear_tree(): #빈 tree 출력
     tree = tkinter.ttk.Treeview(win, columns=["one", "two", "three", "four", "five"],
                                 displaycolumns=["one", "two", "three", "four", "five"], height=27)
