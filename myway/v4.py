c3e542e36c52f9d891f426904d9b729d196c9c15
DK <dongwook5478@gmail.com>
2022-12-13 13:22:48 +0900

    change test.xl, extends num of sheets to 5

--- myway/v4.py
+++ myway/v4.py
@@ -49,25 +49,26 @@
 import openpyxl
 import subprocess
 
-def setog_sheets(): #왼쪽 시트별 길이 저장 =>og_row(3개 기준)
+def og_sheets_row(): #왼쪽 시트별 길이 저장 =>og_row(3개 기준)
     count = 0
     for i in range(len(og_sheets)):
         for rows in og_sheets[i].iter_rows():  # ws시트 row 길이를 count에 저장
             count += 1
         og_row[i]=count
         count=0
-
-def setlist(): #og_l에 셀 값 저장(2개)
+def setlist(): #셀 값 저장 => og_l 시트 3개 기준(column 2개)
+    og_sheets_row() #사용할때마다 row를 다시 구한다
     row=[]
     for i in range(len(og_l)):
          for k in range(1, (og_row[i] + 1)):  # og_l[i] row 길이만큼 반복
-            for j in range(1, 3):
-                row.append(og_sheets[i].cell(row=k, column=j).value)
+            for j in range(1, 3): #column 1,2만 저
+                row.append(og_sheets[i].cell(k,j).value)
             og_l[i].append(row)
             row = []
 
     for i in range(len(og_l)):
         print(og_l[i])
+
 
 #시트기준
 #Sheet1= peersonal info devied by row
@@ -82,27 +83,29 @@ def setlist(): #og_l에 셀 값 저장(2개)
 # ahn1
 # ahn2
 # ahn3
-home = '/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/test.xlsx' #
-room1='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_one.xlsx'
-room2='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_two.xlsx'
-room3='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_three.xlsx'
-room5='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_five.xlsx'
-room6='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_six.xlsx'
+
+if __name__ == "__main__":
+    home = '/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/test.xlsx' #
+    room1='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_one.xlsx'
+    room2='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_two.xlsx'
+    room3='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_three.xlsx'
+    room5='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_five.xlsx'
+    room6='/Users/doungukkim/Desktop/workspace/python/restinpeace/myway/excel/room_six.xlsx'
 
 
-og_file= openpyxl.load_workbook(home, data_only=True) #초기 시트 위치 저장(값으로)
+    og_file= openpyxl.load_workbook(home, data_only=True) #초기 시트 위치 저장(값으로)
 
-og_sheets=[og_file['Sheet1'],og_file['Sheet2'],og_file['Sheet3']]  #시트 리스트에 저장 시트 이름 바꾸면 같이 바꿔야 함
-og_row=['','',''] #길이 저장
+    og_sheets=[og_file['식당판매'],og_file['매점판매'],og_file['장의용품'],og_file['상복'],og_file['기타']]  #시트 리스트에 저장 시트 이름 바꾸면 같이 바꿔야 함
+    og_row=['','','','',''] #길이 저장
 
-og_l=[[],[],[]]
-new_l=[]
+    og_l=[[],[],[],[],[]] #column 2개에 있는 cell info each list에 저장
+    new_l=[]
 
-og_p=[]
-new_p=[]
+    og_p=[]
+    new_p=[]
 
-setog_sheets()
+    og_sheets_row()
 
-for i in range(3):
-    print(og_row[i])
-setlist()
+    for i in range(5):
+        print(og_row[i])
+    setlist()
